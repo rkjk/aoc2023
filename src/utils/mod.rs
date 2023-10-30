37a259aa5ff665d034fe9cc848f6156fdfb863d4
Raghav Kuppan <raghav.kuppan@outlook.com>
2023-10-30 22:51:37 +0530

    Carry over common functions

---
+++ src/utils/mod.rs
@@ -0,0 +1,9 @@
+use std::io::{BufRead, BufReader, ErrorKind};
+
+pub fn read_input(filename: &str) -> Result<Vec<String>, Error> {
+    let f = File::open(filename).unwrap();
+    let f = BufReader::new(f);
+    f.lines()
+        .map(|l| l.and_then(|v| v.parse().map_err(|e| Error::new(ErrorKind::InvalidData, e))))
+        .collect()
+}
