ac29c238710fb3d9e708f02beb93c057c3b2eb06
Raghav Kuppan <raghav.kuppan@outlook.com>
2023-12-02 22:38:28 +0530

    Remove boilerplate

--- src/lib.rs
+++ src/lib.rs
@@ -3,18 +3,3 @@
 mod aoc1;
 mod aoc2;
 mod utils;
-
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
-}
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
-}
